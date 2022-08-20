fdc0efe4c3ab88a18d9df35058b785473bf71d04
KirillGorilo <kirill.007.228.74@gmail.com>
2022-08-20 12:49:01 +0900

    error correction

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path("list/add/", views.ListCreate.as_view(), name="list-add"),
     path(
         "list/<int:pk>/delete/", views.ListDelete.as_view, name="list-delete"
-    )
+    ),
     path(
         "list/<int:list_id>/item/add/",
         views.ItemCreate.as_view(),
