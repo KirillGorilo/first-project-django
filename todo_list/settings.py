0eb20a829af0894492d603d6dd959d593c9f7657
KirillGorilo <kirill.007.228.74@gmail.com>
2022-08-20 13:04:40 +0900

    error correction

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY', default='')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
